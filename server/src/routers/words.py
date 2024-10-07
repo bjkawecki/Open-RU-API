a1dbc27d48f939ef3f7b5bef8723bd1079a1b6f3
bjoernkawecki <121129764+bjkawecki@users.noreply.github.com>
2024-10-07 15:41:45 +0200

    add routers folder

---
+++ server/src/routers/words.py
@@ -0,0 +1,47 @@
+from fastapi import Depends, Path, HTTPException, APIRouter
+from typing import List, Annotated
+from sqlmodel import Session, select
+
+from src.models.word import Word, WordBase
+from src.db import get_session
+
+router = APIRouter()
+
+
+@router.post("/words/", response_model=Word)
+async def create_word(word_data: WordBase, session: Session = Depends(get_session)) -> Word:
+    word = Word(**word_data.dict())
+    session.add(word)
+    session.commit()
+    session.refresh(word)
+    return word
+
+
+@router.get("/")
+async def get_index() -> str:
+    return "Welcome to Open Ru API"
+
+
+@router.get("/words/", response_model=List[Word])
+async def get_words(skip: int = 0, limit: int = 100, session: Session = Depends(get_session)):
+    words = session.exec(select(Word)).all()
+    return words
+
+
+@router.get("/words/{word_id}")
+async def get_word_details(
+    word_id: Annotated[int, Path(title="Word ID")], session: Session = Depends(get_session)
+) -> Word:
+    word = session.get(Word, word_id)
+    if word is None:
+        raise HTTPException(status_code=404, detail="Word not found.")
+    return word
+
+
+@router.delete("/words/")
+async def delete_words(session: Session = Depends(get_session)):
+    words = session.exec(select(Word)).all()
+    for word in words:
+        session.delete(word)
+        session.commit()
+    return {"message": "Words successfully deleted."}
--- server/src/server.py
+++ server/src/server.py
@@ -1,16 +1,10 @@
-from fastapi import FastAPI, Depends, Path, HTTPException
-from typing import List, Annotated
-from sqlmodel import Session, select
+from fastapi import FastAPI
+from src.routers import words
 from fastapi.middleware.cors import CORSMiddleware
-
-from src.models.word import Word, WordBase
-from src.db import get_session
-
-
-app = FastAPI()
 
 origins = ["http://localhost:3000", "http://localhost:8000", "http://172.20.0.3:3000/"]
 
+app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
     allow_origins=origins,
@@ -18,42 +12,4 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"],
 )
-
-
-@app.post("/words/", response_model=Word)
-async def create_word(word_data: WordBase, session: Session = Depends(get_session)) -> Word:
-    word = Word(**word_data.dict())
-    session.add(word)
-    session.commit()
-    session.refresh(word)
-    return word
-
-
-@app.get("/")
-async def get_index() -> str:
-    return "Welcome to Open Ru API"
-
-
-@app.get("/words/", response_model=List[Word])
-async def get_words(skip: int = 0, limit: int = 100, session: Session = Depends(get_session)):
-    words = session.exec(select(Word)).all()
-    return words
-
-
-@app.get("/words/{word_id}")
-async def get_word_details(
-    word_id: Annotated[int, Path(title="Word ID")], session: Session = Depends(get_session)
-) -> Word:
-    word = session.get(Word, word_id)
-    if word is None:
-        raise HTTPException(status_code=404, detail="Word not found.")
-    return word
-
-
-@app.delete("/words/")
-async def delete_words(session: Session = Depends(get_session)):
-    words = session.exec(select(Word)).all()
-    for word in words:
-        session.delete(word)
-        session.commit()
-    return {"message": "Words successfully deleted."}
+app.include_router(words.router)
